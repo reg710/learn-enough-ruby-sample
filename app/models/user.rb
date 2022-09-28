8c345c2b1a5beac0087897c3578e32cae1027f40
rcon <reg710@gmail.com>
2022-09-28 18:03:46 -0400

    Add email validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+    # constants are indcated with all caps
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    
     # These two expressions are doing the same thing
     # validates(:name, { presence: true } )
-    validates :name, presence: true
+    # validates :name, presence: true
+
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,4 +15,34 @@ class UserTest < ActiveSupport::TestCase
     refute @user.valid?
   end
 
+  test "email should be present" do
+    @user.email = "   "
+    refute @user.valid?
+  end
+
+  test "name should be under 50 chars" do
+    @user.name = "a" * 51
+    refute @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    refute @user.valid?
+  end
+
+  test "email validation should accept valid emails" do
+    valid_addresses = %w[user@example.com TEST@run.edu trial_snake-case@place.co.uk]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should rejected invalid emails" do
+    invalid_addresses = %w[user@example,com user_at_work.org user.name@example. user@test+run..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      refute @user.valid? "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
