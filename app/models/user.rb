09153918c651173617c0926df36978146b70ea9e
rcon <reg710@gmail.com>
2022-10-01 18:47:28 -0400

    Add password digest to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
 
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX}, uniqueness: true
+
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "secure123", password_confirmation: "secure123")
   end
 
   test "should be valid" do
@@ -14,17 +14,27 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "   "
     refute @user.valid?
   end
-
+  
   test "email should be present" do
     @user.email = "   "
     refute @user.valid?
   end
+  
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
 
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+  
   test "name should be under 50 chars" do
     @user.name = "a" * 51
     refute @user.valid?
   end
-
+  
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     refute @user.valid?
