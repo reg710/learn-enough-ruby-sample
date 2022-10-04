c6ef1bdad0447b0ca12f585f925b635b24229bba
rcon <reg710@gmail.com>
2022-10-03 21:02:34 -0400

    Add gravatars and help function to find them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    # Using the byebug gem, this will pause in the console 
+    # allowing you to interactively test things out 
+    # debugger
   end
-  
+
   def new
+    debugger
   end
 end
