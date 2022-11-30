8b7096402b11314e4d0a8d7aa2e45e47c172ded4
rcon <reg710@gmail.com>
2022-11-29 19:18:09 -0500

    add error message display 7.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+
     # Using the byebug gem, this will pause in the console 
     # allowing you to interactively test things out 
     # debugger
@@ -10,4 +11,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    # This is not permitted by default because
+    # it would allow user to send in any data to user model
+    # use strong parameters as below
+    # @user = User.new(params[:user])
+
+    @user = User.new(user_params)
+    if @user.save
+      #handle a successful save
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
