d8de97b6187a8b8d09dbc36db222442648126150
rcon <reg710@gmail.com>
2022-12-01 14:06:58 -0500

    Create sessions controller. 8.1.4

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log the user in and redirect to the user's show page
+    else
+      # create error message
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
