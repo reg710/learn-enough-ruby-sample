18295fd9a799c569b2f6c3f0878f19f134cf9bad
rcon <reg710@gmail.com>
2022-12-12 19:37:17 -0500

    Add remember method to user 9.1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
+
     before_save { email.downcase! }
 
     # constants are indcated with all caps
@@ -14,10 +16,22 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
 
-    # Returns the hash digest of the given string
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
+    class << self
+        # think of above as different way of writing def User.digest(string)
+        # Returns the hash digest of the given string
+        def digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+        end
+    
+        # Returns a random toke
+        def new_token
+            SecureRandom.urlsafe_base64
+        end
     end
 
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
 end
