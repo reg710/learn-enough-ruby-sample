756b3f8493344f2090b903c54b2aa537ad0c88dc
rcon <reg710@gmail.com>
2023-01-21 17:24:00 -0500

    Add pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,22 @@
 # Create a main sample user
 
-User.create!(name: 'Example User', 
-    email: "example@tutorial.org",
-    password: "password",
-    password_confirmation: "password"
+User.create!(
+  name: "Example User",
+  email: "example@tutorial.org",
+  password: "password",
+  password_confirmation: "password",
 )
 
 # Generate a bunch of additional users
 99.times do |n|
-    name = Faker::Name.name
-    email = "example-#{n+1}@tutorial.org"
-    password = "password"
-    User.create!(name: name, 
-        email: email, 
-        password: password, 
-        password_confirmation: password
-    )
+  name = Faker::Name.name
+  email = "example-#{n + 1}@railstutorial.org"
+  password = "password"
+  # The ! means it will raise an exception for an invalid user rather than returning false
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+  )
 end
