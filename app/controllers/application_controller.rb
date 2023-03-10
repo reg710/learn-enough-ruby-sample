a1af8b033ca80d4b0f90de3de6df68928f271c54
rcon <reg710@gmail.com>
2023-03-09 19:32:19 -0500

    Create mircopost form and initial tests. 13.3.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
-    # By including the helper at this level, all controllers will have access to it
-    include SessionsHelper
-    def hello
-        render html: "hello, world!"
+  # By including the helper at this level, all controllers will have access to it
+  include SessionsHelper
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url, status: :see_other
     end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,14 +61,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
